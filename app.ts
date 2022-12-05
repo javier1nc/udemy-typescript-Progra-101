8886119588f7a661e0c50d415185346f86d4dba1
Javier Sánchez Mendoza <code@javier1nc.com>
2022-12-05 05:03:53 +0000

    commit

--- app.ts
+++ app.ts
@@ -1,4 +1,13 @@
 
-console.log('Hola Mundo!!');
+let numberOfLines = 1;
 
+console.log('Linea #', numberOfLines);
 
+numberOfLines ++;
+console.log('Linea #', numberOfLines);
+
+numberOfLines = numberOfLines + 1;
+console.log('Linea #', numberOfLines);
+
+numberOfLines += 1;
+console.log('Linea #', numberOfLines);
